af8d6ccf84db21091028e90251d5f35415678d57
Adam B. Cellon <adam.cellon@gmail.com>
2017-04-15 16:28:05 -0400

    chbnn_test.sh changed

--- chbnn_test.sh
+++ chbnn_test.sh
@@ -4,16 +4,16 @@
 #SBATCH --output=chbnn.py.o%j
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
-#SBATCH --time=00:4:00
+#SBATCH --time=00:10:00
 #SBATCH --workdir=~/thesis
 #SBATCH --gres=gpu:1
 
 # can remove following line if it is in your ~/.bash_profile etc
 export MODULEPATH=/tigress/PNI/modulefiles:$MODULEPATH
 
-module load python/anaconda/4.0.0
+module load anaconda
 module load cudatoolkit
 module load intel-mkl
 source activate /tigress/acellon/theano
-python ./chbnn.py chb01 50 True False
+python ./chbnn.py chb01 100 True
 
