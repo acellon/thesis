62bc566e7d4185c08a5f87cfff3b40dc0e6e38bf
Adam B. Cellon <adam.cellon@gmail.com>
2017-05-05 00:14:40 -0400

    chb09 test

--- scripts/chbnn_gen.sh
+++ scripts/chbnn_gen.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 
 #SBATCH --job-name=chbnn_gen.py
-#SBATCH --output=chb08testlw.o%j
+#SBATCH --output=chb09testlw.o%j
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
 #SBATCH --time=01:00:00
 #SBATCH --workdir=/home/acellon/thesis
 #SBATCH --gres=gpu:1
-#SBATCH --mem=12GB
+#SBATCH --mem=32GB
 #SBATCH --mail-type=end
 #SBATCH --mail-user=acellon@princeton.edu
 
@@ -18,5 +18,5 @@ module load anaconda
 module load cudatoolkit
 module load intel-mkl
 source activate /tigress/acellon/theano
-echo 'CHB08 simple test of loowin (thresh=0.75, learning_rate=1e-4)'
-python ./chbnn_gen.py chb08 2 0.75 True testlw
+echo 'CHB09 simple test of loowin (thresh=0.75, learning_rate=1e-4)'
+python ./chbnn_gen.py chb09 2 0.75 True testlw
