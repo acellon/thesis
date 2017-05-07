ca57d8220de790fadf8dbebd8a395eb1f0fbb799
Adam B. Cellon <adam.cellon@gmail.com>
2017-05-07 00:52:16 -0400

    second round of fcl tests

--- scripts/chbnn_gen.sh
+++ scripts/chbnn_gen.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-#SBATCH --job-name=chb05fcl2
-#SBATCH --output=chb05fcl2.o%j
+#SBATCH --job-name=chb05fcl9
+#SBATCH --output=chb05fcl9.o%j
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
 #SBATCH --time=04:00:00
@@ -18,6 +18,6 @@ module load anaconda
 module load cudatoolkit
 module load intel-mkl
 source activate /tigress/acellon/theano
-echo 'CHB05 thresh=0.8, osr=6, usp=0.3, 100 fcl units'
+echo 'CHB05 thresh=0.8, osr=6, usp=0.3, 129 fcl units'
 #          subj num_epochs thresh osr usp tiger tag
-python ./chbnn_gen.py chb05 50 0.8 6 0.3 True fcl2
+python ./chbnn_gen.py chb05 50 0.8 6 0.3 True fcl9
